bf9575a530e6ed2d327923ec3e5e52626cc67418
NatanielSilva <nataniel.silva@icloud.com>
2021-01-01 11:46:17 -0300

    Ajustes 2021

--- Deploy.sh
+++ Deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Sudo su'
+echo 'sudo su'
 sudo su
 
 echo 'update'
@@ -25,22 +25,32 @@ sudo apt-get install wget -y
 echo 'Instalando Chrome'
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm -rf *.deb
 
 echo 'Instalando terminator'
 sudo apt-get install terminator -y
 
-echo 'Instalando Teamviewr'
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
-sudo apt install -y ./teamviewer_amd64.deb
+#echo 'Instalando Teamviewr'
+#wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+#sudo apt install -y ./teamviewer_amd64.deb
 
 echo 'Instalando Docker'
-curl -fsSL https://get.docker.com/ | bash
-chmod 777 /var/run/docker.sock
-docker run hello-world
+curl -fsSL https://get.docker.com/ | sh
+systemctl start docker
+systemctl status docker
+systemctl enable Docker
 
-echo 'Instalando SKype'
-wget https://go.skype.com/skypeforlinux-64.deb
-dpkg -i skypeforlinux-64.deb
+#Adicionar usuário ao grupo do docker
+usermod -aG docker <usuario>
+2. DOCKER-COMPOSE
+
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-
+$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#echo 'Instalando SKype'
+#wget https://go.skype.com/skypeforlinux-64.deb
+#dpkg -i skypeforlinux-64.deb
 
 echo 'Instalando Filezila'
 sudo add-apt-repository ppa:n-muench/programs-ppa
@@ -48,10 +58,14 @@ sudo apt-get update
 sudo apt-get install filezilla
 
 echo 'Instalando VisualCode'
-sudo snap install --classic code
+sudo apt install software-properties-common apt-transport-https wget
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add --
+sudo add-apt-repository “deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main”
+sudo apt install code
 
-echo 'instalando spotify'
-snap install spotify
+
+#echo 'instalando spotify'
+#snap install spotify
 
 echo 'instalando git'
 sudo apt install git -y
